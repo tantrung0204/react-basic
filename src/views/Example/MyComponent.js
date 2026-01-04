389e1fd7c54937b0dc21af8e099805dea6ba3e00
tantrung0204 <trungntce200064fpt@gmail.com>
2026-01-04 20:49:17 +0700

    DOM Events

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,9 +1,45 @@
 import React from 'react';
 
 class MyComponent extends React.Component {
+
+    // Key: value
+    // state nó là 1 object
+    state = {
+        name: "Jun",
+        channel: "Jun Nguyen"
+    }
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleClickButton = () => {
+        alert("Click me")
+    }
+
+
+    /*
+    JSX => return a block of HTML tags
+    */
     render() {
+
+
         return (
-            <div>Hello my component Jun Jun</div>
+            <>
+                <div className='first'>
+                    <input value={this.state.name} type='text'
+                        onChange={(event) => this.handleOnChangeName(event)} /> <br />
+                    Hello my component, My name is {this.state['name']}
+                </div>
+                <div className='second'>
+                    My channel is: {this.state.channel}
+                </div>
+                <div className='third'>
+                    <button onClick={() => this.handleClickButton()}>Click me</button>
+                </div>
+            </>
         )
     }
 }
