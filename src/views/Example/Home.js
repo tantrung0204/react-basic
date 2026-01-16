cac5f5a661d304858c92f5f839a559a77f181a70
tantrung0204 <trungntce200064fpt@gmail.com>
2026-01-16 20:14:02 +0700

    mapping state to props

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,23 +1,30 @@
 import React from "react";
 import Color from "../HOC/Color";
 import logo from "../../assets/images/clublogo.png"
+import { connect } from "react-redux";
 
 class Home extends React.Component {
 
 
     render() {
-
+        console.log('>>> check props redux: ', this.props.dataRedux)
         return (
             <>
                 <div>
                     Hello world from homepage with Jun Nguyen
                 </div>
                 <div>
-                    <img src={logo} style={{ width: '400px', height: '400px', margin: '10px' }} />
+                    <img src={logo} style={{ width: '200px', height: '200px', margin: '10px' }} />
                 </div>
             </>
         )
     }
 }
 
-export default Color(Home);
+const mapStateToProps = (state) => {
+    return {
+        dataRedux: state.users
+    }
+}
+
+export default connect(mapStateToProps)(Color(Home));
